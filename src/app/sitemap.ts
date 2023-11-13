b9da8b49d6c28a7d3d348614ec2e24e71b02d695
lusaib-netstager <lusaib.latheef@netstager.com>
2023-11-13 17:32:12 +0530

    Changes

---
+++ src/app/sitemap.ts
@@ -0,0 +1,37 @@
+import { MetadataRoute } from "next";
+import { parseString } from "xml2js";
+
+type StoreUrls = {
+  id: number;
+  name: string;
+  slug: string;
+  image_url: string;
+  store_url_analytics: string;
+};
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const result = await fetch(
+    "https://admin.nestooffers.com/api/get-store-urls",
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "text/xml",
+      },
+    }
+  );
+
+  if (!result.ok) {
+    throw new Error(`Failed to fetch sitemap. Status: ${result.status}`);
+  }
+
+  const xmlObject: StoreUrls[] = (await result.json())?.urls;
+  // Using xml2js to parse the XML string
+  const parsedSitemap: MetadataRoute.Sitemap = (xmlObject || [])?.map((e) => ({
+    url: e.store_url_analytics,
+    lastModified: new Date(),
+    changeFrequency: "yearly",
+    priority: 1,
+  }));
+
+  return parsedSitemap;
+}
